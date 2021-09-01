a2b7758f9b02dcd3f7d1b431085246048b78c154
petronalla <petronallaawuor@gmail.com>
2021-09-01 16:22:03 +0400

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Prints out the absolute value.
+ *
+ * @n: Placeholder for the integer input.
+ *
+ * Return: The absolute value.
+ */
+
+int _abs(int n)
+
+{
+if (n >= 0)
+	return (n);
+else
+	return (-n);
+}
+
+
