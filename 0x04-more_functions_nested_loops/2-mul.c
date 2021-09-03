02b3a3b3a67fcb1f68efe38ba5d4bfc56a14473a
petronalla <petronallaawuor@gmail.com>
2021-09-03 20:55:02 +0400

    mulc

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ *
+ * @a: First integer.
+ *
+ * @b: Second integer.
+ *
+ * Return: Returns the product.
+ */
+int mul(int a, int b)
+{
+	int multy;
+
+	multy = (a * b);
+	return (multy);
+}
