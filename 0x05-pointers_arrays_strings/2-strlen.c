ca88e1e593e14dc8f8dfdd9d6c73d80f1d0524ff
petronalla <petronallaawuor@gmail.com>
2021-09-07 20:39:39 +0400

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of @s.
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s++)
+	{
+		l++;
+	}
+
+	return (l);
+}
+
