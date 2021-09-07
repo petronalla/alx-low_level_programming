b6cc68dccb63ddb85a49552b87b5c3a50b4ed06f
petronalla <petronallaawuor@gmail.com>
2021-09-07 19:47:05 +0400

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer argument and
+ *		 dereferences it's value to 98.
+ *
+ * @n: Pointer argument passed to function.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
