36f1b4437cd2a8e60e40b66731b695a19600b118
petronalla <petronallaawuor@gmail.com>
2021-09-14 21:16:43 +0400

    password

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string.
- * @s:
- * @c:
- * Return: c in the string s or NULL.\
+ * _strchr - Locates character in string.
+ *
+ * @s: Pointer to string where character is located from.
+ * @c: Character to be located.
+ *
+ * Return: Pointer to first occurrence of @c in @s,
+ *	   NULL if character is not found.
  */
-
 char *_strchr(char *s, char c)
 {
-        
-	    int i;
+	int i = 0;
+	char *p = s;
 
-	    for (i = 0; *(s + i); i++)
-	    {
-		if (*(s + i) == c)
-            {
-			return (s + i);
-            }    
-	    }
-	    if (*(s + i) == c)
-        {
-		        return (s + i);
-        }       
-	return (0);
+	while (p[i] >= '\0')
+	{
+		if (p[i] == c)
+		{
+			return (p + i);
+		}
+		i++;
+	}
+	return ('\0');
 }
+
+
